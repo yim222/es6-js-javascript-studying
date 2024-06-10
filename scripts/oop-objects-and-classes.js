aa02d37a9c37def88f163759f29f96659ab60be6
lingar <yimprogramming@gmail.com>
2024-06-10 05:00:18 +0300

    Some basic objects

--- scripts/oop-objects-and-classes.js
+++ scripts/oop-objects-and-classes.js
@@ -14,6 +14,11 @@ const stock = {
     },
     addPrice(multi){
         this.price += multi;
+    },
+
+    changeName(newStock){
+        stock.sign = newStock;// work
+        // this.sign = newStock;
     }
 
 
@@ -28,6 +33,13 @@ const changeAndShow = () =>{
     elements[1].innerHTML = stock.price;
 
 
+}
+const updateView = ()=>{
+    let elements = document.querySelector("#stock2").children;
+    elements[0].innerHTML = stock["sign"];
+    elements[1].innerHTML = stock.price;
+    elements[2].innerHTML = stock["officialName"];
+    elements[3].innerHTML = stock.max;
 }
 
 const addNewProps = () =>{
@@ -46,3 +58,13 @@ let elements = document.querySelector("#stock1").children;
 
 elements[0].innerHTML = stock.sign;
 elements[1].innerHTML = stock.price;
+
+// Notification.permission;
+const myNotification = new Notification("Hello!");
+// myNotificat
+
+const changeName=(ev)=>{
+    // console.log(ev.getElementsByName("button").innerHTML);
+    console.log("hi")
+    stock.changeName("AAPL");
+}
